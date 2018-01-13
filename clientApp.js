767f72ddcfdea7ee01fcfde92305d3281b2b2d92
SamKahnke <samkahnke@gmail.com>
2018-01-13 15:11:06 -0600

    replace temp transcript button

--- clientApp.js
+++ clientApp.js
@@ -1,14 +1,15 @@
 $(function() {
-            $( "#transcript-dialog" ).dialog({
-               autoOpen: false,
-               position: {
-                 at: "left bottom"
-               },
-               hide: "slide",
-               show : "slide",
-               height: 400
-            });
-            $( "#transcript-opener" ).click(function() {
-               $( "#transcript-dialog" ).dialog( "open" );
-            });
-         });
+   $( "#transcript-dialog" ).dialog({
+      autoOpen: false,
+      position: {
+        at: "left bottom"
+      },
+      hide: "slide",
+      show : "slide",
+      height: 400
+   });
+
+   $( "#transcript-btn" ).click(function() {
+      $( "#transcript-dialog" ).dialog( "open" );
+   });
+});
