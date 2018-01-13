bb56f2445feea04066b93116953c7c544a69e750
patkahnke <pat.kahnke@banno.com>
2018-01-13 14:19:15 -0600

    adds transcript dialog

---
+++ clientApp.js
@@ -0,0 +1,14 @@
+$(function() {
+            $( "#transcript-dialog" ).dialog({
+               autoOpen: false,
+               position: {
+                 at: "left bottom"
+               },
+               hide: "slide",
+               show : "slide",
+               height: 400
+            });
+            $( "#transcript-opener" ).click(function() {
+               $( "#transcript-dialog" ).dialog( "open" );
+            });
+         });
