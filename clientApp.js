0028b608b89b9170f53874b9359bc230349c022e
SamKahnke <samkahnke@gmail.com>
2018-01-13 18:34:59 -0600

    disable dialog animation when not touching window left

--- clientApp.js
+++ clientApp.js
@@ -1,19 +1,40 @@
 $(function() {
-   $( "#transcript-dialog" ).dialog({
+   const transcript = $( "#transcript-dialog" );
+   const transcriptButton = $( "#transcript-btn" );
+
+   transcript.dialog({
       autoOpen: false,
-      position: {
-        at: "left bottom"
-      },
+      position: { at: "left bottom" },
       hide: "slide",
-      show : "slide",
-      height: 400
+      show: "slide",
+      height: 400,
+      width: 340,
+      dragstop: function( event, ui ) {},
+      resizestop: function( event, ui ) {}
    });
 
-   $( "#transcript-btn" ).click(function() {
-      if ($( "#transcript-dialog" ).dialog( "isOpen" )) {
-         $( "#transcript-dialog" ).dialog( "close" );
+   transcript.on( "dialogdragstop dialogresizestop", function( event, ui ) {
+      setEffect();
+   });
+
+   transcriptButton.click(function() {
+      let isOpen = transcript.dialog( "isOpen" );
+
+      if (isOpen) {
+         transcript.dialog( "close" );
       } else {
-         $( "#transcript-dialog" ).dialog( "open" );
+         transcript.dialog( "open" );
       }
    });
+
+   // enables slide effect if dialog is touching the window's left side
+   function setEffect () {
+      let pos = transcript.dialog( "option", "position" ).at;
+      
+      if (pos.includes("left ") || pos.includes("left+0")) {
+         transcript.dialog( "option", { hide: "slide", show: "slide" } );
+      } else {
+         transcript.dialog( "option", { hide: false, show: false } );
+      }
+   }
 });
